30ef4a03b72a0db995278023281c8d3f8357de4d
Jason Byer <jbyer3@gmail.com>
2018-11-03 19:40:13 -0400

    production change

--- models/index.js
+++ models/index.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.set("useFindAndModify", false);
 mongoose.connect(
-  "mongodb://localhost/factory-tree",
+  // "mongodb://passport:db4passport@ds151383.mlab.com:51383/treeview4passport",
+  process.env.MONGODB_URI || "mongodb://localhost/factory-tree",
   { useNewUrlParser: true }
 );
 
